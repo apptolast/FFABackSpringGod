2e0f03670534e223f4f6a8757618df48f6366c67
Pablo Hurtado Gonzalo <pablohurtadohg@gmail.com>
2024-09-19 17:31:28 +0200

    Fix Endpoint Update User AND go for delete

--- src/main/java/com/ffa/back/dto/UserUpdateRequestDTO.java
+++ src/main/java/com/ffa/back/dto/UserUpdateRequestDTO.java
@@ -7,13 +7,16 @@ public class UserUpdateRequestDTO {
     @Pattern(regexp = "^[a-z]{2}$", message = "Language must be a 2-letter code")
     private String language;
 
+    private String email;
+
     // Constructor sin argumentos
     public UserUpdateRequestDTO() {
     }
 
     // Constructor con argumentos (opcional)
-    public UserUpdateRequestDTO(String language) {
+    public UserUpdateRequestDTO(String language, String email) {
         this.language = language;
+        this.email = email;
     }
 
     // Getter y Setter
@@ -23,5 +26,13 @@ public class UserUpdateRequestDTO {
 
     public void setLanguage(String language) {
         this.language = language;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
