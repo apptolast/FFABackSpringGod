a09e7f86507d50437ced1e915b8d3979f8575814
Pablo Hurtado Gonzalo <pablohurtadohg@gmail.com>
2024-10-11 11:02:20 +0200

    Working in Redis Cache System with Docs Test

--- src/main/java/com/ffa/back/config/AppConfig.java
+++ src/main/java/com/ffa/back/config/AppConfig.java
@@ -2,6 +2,10 @@ package com.ffa.back.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -10,5 +14,15 @@ public class AppConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration("redis", 6379));
+    }
+
+    @Bean
+    public ReactiveRedisTemplate<String, String> reactiveRedisTemplate(LettuceConnectionFactory connectionFactory) {
+        return new ReactiveRedisTemplate<>(connectionFactory, RedisSerializationContext.string());
     }
 }
