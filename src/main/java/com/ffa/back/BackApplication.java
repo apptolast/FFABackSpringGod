82c01e1f0843293c8fd7350833ca0a8c0832d847
Pablo Hurtado Gonzalo <pablohurtadohg@gmail.com>
2024-10-11 09:10:38 +0200

    Working in Redis Cache System

--- src/main/java/com/ffa/back/BackApplication.java
+++ src/main/java/com/ffa/back/BackApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableConfigurationProperties(FirebaseProperties.class)
 @EntityScan(basePackages = {"com.ffa.back.models"})
+@EnableCaching
 public class BackApplication {
 
 	public static void main(String[] args) {
