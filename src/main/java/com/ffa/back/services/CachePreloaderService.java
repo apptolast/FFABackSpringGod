0401016f6642dbbc3ff5cbba3e7fbe0d00e41605
Pablo Hurtado Gonzalo <pablohurtadohg@gmail.com>
2024-10-15 11:23:05 +0200

    FFABS-2 Cache Working and Preloading tasks

--- src/main/java/com/ffa/back/services/CachePreloaderService.java
+++ src/main/java/com/ffa/back/services/CachePreloaderService.java
@@ -1,11 +1,16 @@
 package com.ffa.back.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CachePreloaderService {
+
+    private static final Logger log = LoggerFactory.getLogger(CachePreloaderService.class);
+
 
     @Autowired
     private TmdbService tmdbService;
@@ -15,6 +20,7 @@ public class CachePreloaderService {
      */
     @Scheduled(fixedRate = 60 * 60 * 1000) // Cada hora
     public void preloadPopularMovies() {
+        log.info("Preloading popular movies...");
         for (int page = 1; page <= 5; page++) {
             tmdbService.getPopularMovies(page).subscribe();
         }
@@ -25,6 +31,7 @@ public class CachePreloaderService {
      */
     @Scheduled(fixedRate = 60 * 60 * 1000) // Cada hora
     public void preloadPopularSeries() {
+        log.info("Preloading popular series...");
         for (int page = 1; page <= 5; page++) {
             tmdbService.getPopularSeries(page).subscribe();
         }
