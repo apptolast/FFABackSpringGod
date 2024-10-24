cb5b841d40adfe5cb8d3ac044c97441c86b76ae8
Pablo Hurtado Gonzalo <pablohurtadohg@gmail.com>
2024-10-24 10:32:14 +0200

    FFABS-4 Reactive Repositories

--- src/main/java/com/ffa/back/repositories/LanguageRepository.java
+++ src/main/java/com/ffa/back/repositories/LanguageRepository.java
@@ -1,12 +1,13 @@
 package com.ffa.back.repositories;
 
 import com.ffa.back.models.Language;
+import com.ffa.back.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
+import reactor.core.publisher.Mono;
 
 @Repository
-public interface LanguageRepository extends JpaRepository<Language, Long> {
-    Optional<Language> findByLanguage(String language);
+public interface LanguageRepository extends ReactiveCrudRepository<Language, Long> {
+    Mono<Language> findByLanguage(String language);
 }
