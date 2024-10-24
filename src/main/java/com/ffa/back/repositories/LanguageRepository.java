d86f3a274d0ac51924e2d304e62dadbaa16e89a8
Pablo Hurtado Gonzalo <pablohurtadohg@gmail.com>
2024-10-24 10:32:14 +0200

    FFABS-4 Reactive Repositories

--- src/main/java/com/ffa/back/repositories/LanguageRepository.java
+++ src/main/java/com/ffa/back/repositories/LanguageRepository.java
@@ -1,10 +1,13 @@
 package com.ffa.back.repositories;
 
 import com.ffa.back.models.Language;
+import com.ffa.back.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
 @Repository
-public interface LanguageRepository extends JpaRepository<Language, Long> {
-    Language findByLanguage(String language);
+public interface LanguageRepository extends ReactiveCrudRepository<Language, Long> {
+    Mono<Language> findByLanguage(String language);
 }
--- src/main/java/com/ffa/back/repositories/UserRepository.java
+++ src/main/java/com/ffa/back/repositories/UserRepository.java
@@ -2,11 +2,13 @@ package com.ffa.back.repositories;
 
 import com.ffa.back.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
+public interface UserRepository extends ReactiveCrudRepository<User, Long> {
+    Mono<User> findByFirebaseUuid(String firebase_uuid);
 }
