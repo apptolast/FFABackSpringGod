32ae0a9453f2679cb2f24c99d21afde474d4ac4d
Coshiloco <pablohurtadohg@gmail.com>
2024-12-02 23:05:34 +0100

    FFABS-13 #12

--- src/main/java/com/ffa/back/services/CacheService.java
+++ src/main/java/com/ffa/back/services/CacheService.java
@@ -2,23 +2,23 @@ package com.ffa.back.services;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 @Service
 public class CacheService {
+    private final RedisTemplate<String, JsonNode> redisTemplate;
 
-    private final ReactiveRedisTemplate<String, JsonNode> reactiveRedisTemplate;
-
-    public CacheService(ReactiveRedisTemplate<String, JsonNode> reactiveRedisTemplate) {
-        this.reactiveRedisTemplate = reactiveRedisTemplate;
+    public CacheService(RedisTemplate<String, JsonNode> redisTemplate) {
+        this.redisTemplate = redisTemplate;
     }
 
-    public Mono<Boolean> cacheData(String key, JsonNode data) {
-        return reactiveRedisTemplate.opsForValue().set(key, data);
+    public void cacheData(String key, JsonNode data) {
+        redisTemplate.opsForValue().set(key, data);
     }
 
-    public Mono<JsonNode> getCachedData(String key) {
-        return reactiveRedisTemplate.opsForValue().get(key);
+    public JsonNode getCachedData(String key) {
+        return redisTemplate.opsForValue().get(key);
     }
 }
