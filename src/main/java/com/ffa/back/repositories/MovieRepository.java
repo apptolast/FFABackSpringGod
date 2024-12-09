267a2421372fc5663d9299bec94cb3b47bc32fa6
Pablo Hurtado Gonzalo <pablohurtadohg@gmail.com>
2024-12-09 09:37:04 +0100

    FFABS-17 Remove comments

--- src/main/java/com/ffa/back/repositories/MovieRepository.java
+++ src/main/java/com/ffa/back/repositories/MovieRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
-    // Aquí puedes agregar métodos de búsqueda personalizados si es necesario
+
 }
